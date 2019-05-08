814e8c7e86295e7f3e3a4309697cce467c5e83bf
Michał Pawłowicz <michal.pawlowicz@eset.pl>
2019-05-08 17:05:17 +0200

    Added modal

--- src/components/MealsView/AddMeal.js
+++ src/components/MealsView/AddMeal.js
@@ -1,48 +1,75 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Modal from '@material-ui/core/Modal';
+import Button from '@material-ui/core/Button';
 
-class Modal extends Component {
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const styles = theme => ({
+  paper: {
+    position: 'absolute',
+    width: theme.spacing.unit * 50,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing.unit * 4,
+    outline: 'none',
+  },
+});
+
+class SimpleModal extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+  handleClose = () => {
+    this.props.updateStateCB(false);
+  };
+
   render() {
-    // The gray background
-    const backdropStyle = {
-      position: 'fixed',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      right: 0,
-      backgroundColor: 'rgba(0,0,0,0.3)',
-      padding: 50
-    };
-
-    // The modal "window"
-    const modalStyle = {
-      backgroundColor: '#fff',
-      borderRadius: 5,
-      maxWidth: 500,
-      minHeight: 300,
-      margin: '0 auto',
-      padding: 30
-    };
+    const { classes } = this.props;
 
     return (
-      <div className="backdrop" style={{backdropStyle}}>
-        <div className="modal" style={{modalStyle}}>
-          {this.props.children}
-          <div className="footer">
-            <button>
-              Close
-            </button>
+      <div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={this.props.showModalAddMeal}
+          onClose={this.handleClose}
+        >
+          <div style={getModalStyle()} className={classes.paper}>
+            <Typography variant="h6" id="modal-title">
+              Text in a modal
+            </Typography>
+            <Typography variant="subtitle1" id="simple-modal-description">
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+            <SimpleModalWrapped />
           </div>
-        </div>
+        </Modal>
       </div>
     );
   }
 }
 
-Modal.propTypes = {
-  //onClose: PropTypes.func.isRequired,
-  show: PropTypes.bool,
-  children: PropTypes.node
+SimpleModal.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-export default Modal;
+const SimpleModalWrapped = withStyles(styles)(SimpleModal);
+
+export default SimpleModalWrapped;
--- src/components/MealsView/MealsView.js
+++ src/components/MealsView/MealsView.js
@@ -2,32 +2,25 @@ import React, { Component } from 'react';
 import ArrowDatePicker from './ArrowDatePicker/ArrowDatePicker';
 import './MealsView.css';
 import MealsContent from './MealsContent/MealsContent';
-import Modal from './AddMeal';
+import SimpleModalWrapper from './AddMeal';
 
 class MealsView extends Component {
     constructor(props) {
         super(props);
         this.state={
-            redirectToAddMeal: false
+            showModalAddMeal: false
         };
-        this.updateState1 = this.updateState1
+        this.updateState1 = this.updateState1;
     }
 
-    updateState1 = (v) => {this.setState({ redirectToAddMeal: true })}
+    updateState1 = (v) => { this.setState(prevState => ({ showModalAddMeal: v })); }
 
     render() {
-        let modal;
-        if(this.state.redirectToAddMeal) {
-            modal = <Modal> Heres some content for the modal </Modal>;
-        } else {
-            modal = <div></div>;
-        }
-        //alert("VAR: " + this.state.redirectToAddMeal)
         return (
             <div className="MealsView">
                 <ArrowDatePicker />
                 <MealsContent updateStateCB={this.updateState1}/>
-                //{modal}
+                <SimpleModalWrapper showModalAddMeal={this.state.showModalAddMeal} updateStateCB={this.updateState1} />
             </div>
         );
     }

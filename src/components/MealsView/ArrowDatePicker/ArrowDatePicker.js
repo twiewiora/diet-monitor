133f5e00a1fef8b1f7257771919f11f1153ca80e
Michal Pawlowicz <michal.pawlowicz@yahoo.com>
2019-05-12 21:30:32 +0200

    date picker on main page

--- src/components/MealsView/ArrowDatePicker/ArrowDatePicker.js
+++ src/components/MealsView/ArrowDatePicker/ArrowDatePicker.js
@@ -11,33 +11,17 @@ import './ArrowDatePicker.css';
 import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
 
+import MaterialUIDayPicker from '../DateAndTimePickers/MaterialUIDayPickers'
+
 class ArrowDatePicker extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            date: new Date()
-        }
-    }
-
-    changeDate(days) {
-        this.state.date.setDate(this.state.date.getDate() + days);
-        this.setState({
-            date: this.state.date
-        })
     }
 
     render() {
       return (
           <div className="ArrowDatePicker">
-              <IconButton onClick={() => this.changeDate(-2)}><DoubleLeft fontSize="large"/></IconButton>
-              <IconButton onClick={() => this.changeDate(-1)}><Left fontSize="large"/></IconButton>
-              <TextField
-                  id="date"
-                  type="date"
-                  value={this.state.date.toISOString().slice(0, 10)}
-              />
-              <IconButton onClick={() => this.changeDate(1)}><Right fontSize="large"/></IconButton>
-              <IconButton onClick={() => this.changeDate(2)}><DoubleRight fontSize="large"/></IconButton>
+            <MaterialUIDayPicker/>
           </div>);
     }
 
